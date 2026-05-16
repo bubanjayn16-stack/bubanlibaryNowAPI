2a6aa4601801f76c20c2338ea26fc3b920c049d8
bubanjayn16-stack <bubanjayn16@gmail.com>
2026-05-16 19:46:56 +0800

    Update Book.cs

--- bubanlibaryNowAPI/Models/Book.cs
+++ bubanlibaryNowAPI/Models/Book.cs
@@ -1,23 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace bubanlibaryNowAPI.Models
+﻿namespace bubanlibaryNowAPI.Models
 {
     public class Book
     {
         public int Id { get; set; }
-
-        [Required]
-        public string Title { get; set; } = string.Empty;
-
-        public string Description { get; set; } = string.Empty;
-
-        [Required]
-        public string Author { get; set; } = string.Empty;
-
-        public string Genre { get; set; } = string.Empty;
-
+        public string? Title { get; set; }
+        public string? Description { get; set; } 
+        public string? Author { get; set; }
+        public string? Genre { get; set; }
         public bool Available { get; set; }
-
         public int PublishedYear { get; set; }
+
+
     }
 }
